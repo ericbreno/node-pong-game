2c9904ede46484ea67e664c82b5dc8e281ec0b2e
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2019-04-20 21:48:59 -0300

    More and more documentation

--- js/keyListenerUtil.js
+++ js/keyListenerUtil.js
@@ -1,8 +1,15 @@
 const readline = require('readline');
 
+/**
+ * Simple subscriber for functions to wait on key press actions.
+ * @param {Function} cb Callback to be registered to listen on pressing of keys.
+ */
 const listeners = [];
 const registerKeyPressListener = cb => listeners.push(cb);
 
+/**
+ * Things needed to activate the listening on pressed keys.
+ */
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.on('keypress', (str, key) => {
--- js/tab.js
+++ js/tab.js
@@ -45,6 +45,9 @@ const makeTab = (_xTab, _yTab, _playerSideSize) => {
         score2 = 0;
 
     return {
+        /**
+         * Clear itens on table, updates the ball to it's next position, increment score (if any player scored), and plot itens on table.
+         */
         updateBall() {
             this.clearItens();
 
@@ -54,23 +57,55 @@ const makeTab = (_xTab, _yTab, _playerSideSize) => {
 
             this.plotItens();
         },
+
+        /**
+         * Builds the data to be printed on terminal (score and table), and prints it.
+         */
         printTab() {
-            cls();
             const scoreStr = `SCORE: ${score1} x ${score2}`;
             const tabStr = tabMatrix.map(line => ` ${line.join('')} `).join('\n');
 
             print(`\n${scoreStr}\n\n${tabStr}`);
         },
+
+        /**
+         * Moves the player 1 in x steps. Positive = move down, negative = move up.
+         * Doesn't let the player move past the table limits.
+         * 
+         * @param {Number} x Steps to move player 1.
+         */
         moveP1(x) {
             this.clearItens();
             p1x = Math.min(yTab - 1 - playerSideSize, Math.max(playerSideSize, p1x + x));
             this.plotItens();
         },
+
+        /**
+         * Moves the player 2 in x steps. Positive = move down, negative = move up.
+         * Doesn't let the player move past the table limits.
+         * 
+         * @param {Number} x Steps to move player 1.
+         */
         moveP2(x) {
             this.clearItens();
             p2x = Math.min(yTab - 1 - playerSideSize, Math.max(playerSideSize, p2x + x));
             this.plotItens();
         },
+
+        /**
+         * Plot itens on table.
+         * Gets the coordinates and char to plot the ball, plot the center of each player
+         * and it's sides.
+         * 
+         * If any player bounced back the ball in the last 2 updates (checked by hitP1 and hitP2),
+         * the tile that bounced back will be plotted differently, for a fancier view.
+         * 
+         * Note that this method is O(playerSideSize * 2 + 4), roughly O(1), it doesn't 
+         * iterates over the entire table.
+         * 
+         * @param {boolean} clear If truthy, erases all current position of itens on tab.
+         *              
+         */
         plotItens(clear = false) {
             const [realBallx, realBally, ballChar] = ballInstance.calculateRealCoordinates();
             tabMatrix[realBallx][realBally] = clear ? defaultCharTab : ballChar;
@@ -91,6 +126,10 @@ const makeTab = (_xTab, _yTab, _playerSideSize) => {
                 tabMatrix[realBallx][xTab - 1] = clear ? defaultCharTab : player2CharCollision;
             }
         },
+
+        /**
+         * Calls #plotItens with true flag
+         */
         clearItens() {
             this.plotItens(true);
         }
